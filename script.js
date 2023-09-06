1e48ead8a796ac7cb2edb54e532afe7b2d9414ba
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-06 12:02:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,4 @@ for (let i = 0; i < 50; i++) {
   for (let j = 0; j < 10; j++) {
     const face = document.createElement("div");
     face.classList.add("face");
+    face.style.transform = `rotateY(${j * 60}deg) translateZ(20px)`;
