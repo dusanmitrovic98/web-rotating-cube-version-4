6ad155511cb3845291741daa6c8428bbd9c54dc4
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-06 11:57:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const scene = document.getElementById("scene");
 // Function to generate a random number between min and max
 function getRandom(min, max) {
   return Math.random() * (max - min) + min;
+}
